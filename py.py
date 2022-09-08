47bd031d3075eb54c37738fdf2f27502a96e70c5
GordeyGudim <gordey.gudim@mail.ru>
2022-09-08 21:17:26 +0300

    install minecraft with python

--- py.py
+++ py.py
@@ -3,7 +3,7 @@ import os
 subprocess.run('sudo pacman -Syu;sudo pacman -S jdk-openjdk', shell=True)
 f = open('Minecraft.sh', 'w')
 f.write('#! /bin/sh\njava -jar TLauncher-2.86.jar')
-subprocess.run('chmod +x Minecraft.sh; ./Minecraft.sh', shell = True)
+subprocess.Popen('chmod +x Minecraft.sh; ./Minecraft.sh', shell = True)
 
 
 
