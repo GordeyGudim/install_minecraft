ff6cb6988f46bb799ae85d17f11dab476b0f8c9e
GordeyGudim <gordey.gudim@mail.ru>
2022-09-08 20:43:33 +0300

    fc

--- py.py
+++ py.py
@@ -1,9 +1,9 @@
 import subprocess
 import os
-subprocess.Popen('sudo pacman -Syu;sudo pacman -S jdk-openjdk')
+subprocess.Popen('sudo pacman -Syu;sudo pacman -S jdk-openjdk', shell=True)
 f = open('Minecraft.sh', 'w')
 f.write('#! /bin/sh\njava -jar TLauncher-2.86.jar')
-subprocess.Popen('chmod +x Minecraft.sh; ./Minecraft.sh')
+subprocess.Popen('chmod +x Minecraft.sh; ./Minecraft.sh', shell = True)
 
 
 
